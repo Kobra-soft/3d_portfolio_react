18a03cf7dece6729b341aaa72070fc21a4a788a4
user1 <code.softwaredevg@gmail.com>
2023-06-28 17:06:15 +0100

    Commit - NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+
 const App = () => {
 
   return (
-    <div className="">
-      3D Developer Portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
