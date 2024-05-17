49e91ad244b75f96ec699f6db2091e5d85bcb0a7
kobra-soft <code.softwaredev@gmail.com>
2024-05-17 10:43:58 +0100

    Debug Fixes / Font Change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,7 @@ const Contact = () => {
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
           ref={formRef}
