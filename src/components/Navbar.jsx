5ec67de5d4362ed82658fcb731c3ef21277413d4
kobra-soft <code.softwaredev@gmail.com>
2024-05-20 22:28:38 +0100

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,23 +33,34 @@ const Navbar = () => {
         scrolled ? "bg-primary" : "bg-transparent"
       }`}
     >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to='/'
-          className='flex items-center gap-2'
+          to="/"
+          className="flex items-center gap-2"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='ml-1 text-white text-[18px] font-bold cursor-pointer flex '>
-            Jack &nbsp;
-            <span className='sm:block hidden'> |&nbsp; Kobra-soft</span>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-10 h-10 rounded-2xl border-[#915eff] border-2"
+          />
+          <p className="ml-1 text-white hover:text-[#915eff] text-[18px] font-bold cursor-pointer flex ">
+            <span className="sm:block hidden text-white hover:text-[#915eff]">
+              Jack
+            </span>
+            <span className="sm:block hidden text-white hover:text-[#ffffff]">
+              &nbsp; |{" "}
+            </span>
+            <span className="sm:block hidden text-white hover:text-[#915eff]">
+              &nbsp; Kobra-soft
+            </span>
           </p>
         </Link>
 
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((nav) => (
             <li
               key={nav.id}
@@ -63,11 +74,11 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain"
             onClick={() => setToggle(!toggle)}
           />
 
@@ -76,7 +87,7 @@ const Navbar = () => {
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
